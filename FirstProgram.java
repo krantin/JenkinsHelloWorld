a4d8784fb229edad7a4d6f2588a2aa5ce6eff5a6
kranthi <krantinarendra@gmail.com>
2020-06-26 15:02:41 -0400

    adding two files realated to FirstProgram

---
+++ FirstProgram.java
@@ -0,0 +1,6 @@
+ public class FirstProgram{
+    public static void main(String[] args) {
+        System.out.println("This is first java program");
+        System.out.println("some modifications in git hub!");
+    }
+}
